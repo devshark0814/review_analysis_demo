ecf8dd3aca8feb869312c3f8ab1632847f6e6761
devshark0814 <devshark0814@gmail.com>
2021-12-26 10:48:41 +0900

    vuetify、vue-toasted、axios導入

--- vue-app/review_analysis_demo/src/main.js
+++ vue-app/review_analysis_demo/src/main.js
@@ -3,13 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
+import Toasted from 'vue-toasted';
 
 Vue.config.productionTip = false
+
+Vue.use(Toasted);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  vuetify,
   components: { App },
   template: '<App/>'
 })
---
+++ vue-app/review_analysis_demo/src/pages/Home/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <v-btn color="primary" @click="doClick">aaa</v-btn>
+    </div>
+</template>
+<script src="./Home.js"></script>
+<style src="./Home.css"></style>
