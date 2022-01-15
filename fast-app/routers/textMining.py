ec4bf3d53e6028c5264044054ec6e176957694dd
SHOHEI_NOTE_PC\devsh <devshark0814@gmail.com>
2022-01-15 13:49:05 +0900

    単純に形態素解析の結果を返すAPIを作成

--- fast-app/routers/textMining.py
+++ fast-app/routers/textMining.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('../')
 from fastapi import APIRouter
 from schemas.textMining import SimpleMecabResultModel
-from Services.MeCab.MeCab import simple_mecab_text
+from Services.MeCab.MeCab import mecab_text
 import itertools
 
 router = APIRouter()
@@ -20,10 +20,4 @@ async def get_simple_mecab_result(req: SimpleMecabResultModel):
     """
     一文を引数でもらい、単純なmecabの解析結果を返す
     """
-    # レビュー毎に二次元配列に形態素解析結果が入っている
-    # two_dic = req.text.apply(mecab_text)
-    two_dic = simple_mecab_text(req.text)
-    # 集計のために一次元配列に
-    # one_dic = list(itertools.chain.from_iterable(two_dic))
-
-    return two_dic
+    return mecab_text(req.text)
